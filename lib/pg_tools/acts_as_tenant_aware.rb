bcccf2df6bb0998894293a9d258ffafcac95aa2d
Michael Fields <mfields1@gmail.com>
2013-08-21 00:34:02 -0400

    Allow schema attribute to be chosen via options hash - resolves #3

--- lib/pg_tools/acts_as_tenant_aware.rb
+++ lib/pg_tools/acts_as_tenant_aware.rb
@@ -1,25 +1,33 @@
 module PgTools
   module ActsAsTenantAware
-    def acts_as_tenant_aware
+    def acts_as_tenant_aware(options = {schema_attribute: :id})
+
+      class_attribute :acts_as_tenant_aware_options
+      self.acts_as_tenant_aware_options = options
+
       class_eval do
         after_create :prepare_tenant 
 
         private
         def prepare_tenant
-          unless PgTools.schemas.include?(id.to_s)
+          unless PgTools.schemas.include?(tenant_schema_name)
             create_schema
             load_tables
           end
         end
 
         def create_schema
-          PgTools.create_schema id
+          PgTools.create_schema tenant_schema_name
         end
 
         def load_tables
-          PgTools.in_search_path(id) { 
+          PgTools.in_search_path(tenant_schema_name) {
             load "#{Rails.root}/db/private/schema.rb"
           }
+        end
+
+        def tenant_schema_name
+          acts_as_tenant_aware_options[:schema_attribute].to_s
         end
       end
     end
